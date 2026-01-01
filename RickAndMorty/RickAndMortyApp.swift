d6100329b67464447c83b17b1e654cd750b21f60
juancbernier <juanberniercr@unimagdalena.edu.co>
2026-01-01 15:41:48 -0500

    Initial commit: Setup SwiftUI project

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Juan Bernier on 1/01/26.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
